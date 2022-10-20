72e8963c7bd4a2e93d778ba424c0f206a286162f
Amy Lo <amyflo@stanford.edu>
2022-10-20 14:31:46 +0100

    remove accenture

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Container, Heading } from "@chakra-ui/react"
 import { StaticImage } from "gatsby-plugin-image"
-import { Link } from "gatsby"
 
 function Hero() {
   return (
@@ -10,15 +9,7 @@ function Hero() {
       <Heading lineHeight="130%" size="xl" fontWeight={400}>
         Amy is a designer and developer obssessed with design systems,
         decision-making, and the brain. She studies psychology and computer
-        science at Stanford. Currently, she works on a frontend team at{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href=" https://www.accenture.com/us-en/about/accenture-song-index"
-        >
-          Accenture Song
-        </a>
-        .
+        science at Stanford.
       </Heading>
     </Container>
   )
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { useState } from "react"
-import { StaticImage } from "gatsby-plugin-image"
 import {
   useColorMode,
   Box,
